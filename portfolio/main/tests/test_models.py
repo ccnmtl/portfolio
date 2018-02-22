380e87ad650e7ed18de3dbcadbbcd4fda62a34fd
Zarina Mustapha <zarina@columbia.edu>
2018-02-22 10:46:14 -0500

    Unit test for HomePage

--- portfolio/main/tests/test_models.py
+++ portfolio/main/tests/test_models.py
@@ -1,6 +1,23 @@
 from django.test import TestCase
+from portfolio.main.tests.factories import EntryFactory
+from portfolio.main.models import HomePage
 
 
-class BasicModelTest(TestCase):
-    def test_dummy(self):
-        assert True
+class TestHomePage(TestCase):
+    def test_entries(self):
+        EntryFactory(live=False, path='0002')
+        e = EntryFactory(path='0003')
+        h = HomePage()
+        qs = h.entries()
+
+        self.assertEquals(qs.count(), 1)
+        self.assertEquals(qs.first(), e)
+
+    def test_featured_entries(self):
+        EntryFactory(feature_on_homepage=False, path='0002')
+        e = EntryFactory(feature_on_homepage=True, path='0003')
+        h = HomePage()
+        qs = h.featured_entries()
+
+        self.assertEquals(qs.count(), 1)
+        self.assertEquals(qs.first(), e)
