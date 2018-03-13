b11dac4db707c3c873750f1e05112e4d2408fcb5
Nik Nyby <nnyby@columbia.edu>
2018-03-13 14:26:34 -0400

    url fix?

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -45,5 +45,5 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
-        path('__debug__/', debug_toolbar.urls),
+        path('__debug__/', include(debug_toolbar.urls)),
     ]
