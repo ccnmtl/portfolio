d122ebbafb6793e4fab3d21a6dc11eb9dad1d235
Zarina Mustapha <zarina@columbia.edu>
2022-03-23 21:54:21 -0400

    Styling for visual index

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -141,7 +141,7 @@ class VisualIndex(Page):
         entries = entries.order_by(sort_order)
 
         # Pagination
-        per_page = 6
+        per_page = 9
         page = request.GET.get('page')
         paginator = Paginator(entries, per_page)
         try:
