fa2d6e0fe36c98d5d9396b7cd808f0ac29de8109
Nikolas Nyby <nikolas@gnu.org>
2024-08-23 15:08:27 -0400

    Update ctlsettings and use init_sentry

--- portfolio/settings_staging.py
+++ portfolio/settings_staging.py
@@ -1,5 +1,6 @@
+from django.conf import settings
+from ctlsettings.staging import common, init_sentry
 from portfolio.settings_shared import *  # noqa: F403
-from ctlsettings.staging import common
 
 BASE_URL = 'https://portfolio.stage.ctl.columbia.edu'
 
@@ -16,3 +17,7 @@ try:
     from portfolio.local_settings import *  # noqa: F403 F401
 except ImportError:
     pass
+
+
+if hasattr(settings, 'SENTRY_DSN'):
+    init_sentry(SENTRY_DSN)  # noqa F405
