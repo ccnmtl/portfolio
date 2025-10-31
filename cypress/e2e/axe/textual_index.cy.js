ec4a4740d751b40e9c8436852fe22b41cea3790c
ndittren <36773036+ndittren@users.noreply.github.com>
2025-10-31 10:25:25 -0400

    Changes for debug toolbar upgrade

--- cypress/e2e/axe/textual_index.cy.js
+++ cypress/e2e/axe/textual_index.cy.js
@@ -7,6 +7,7 @@ describe('Textual Index A11Y', function() {
     it('Tests a11y on default entry page', function() {
         cy.visit('/');
 
+        cy.get('#djHideToolBarButton').click();
         cy.get('a').contains('Our Work').click();
         cy.title().should('contain', 'Our Work -- CTL Portfolio');
 
